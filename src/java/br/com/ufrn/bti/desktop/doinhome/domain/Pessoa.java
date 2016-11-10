9b89a4d0171538788f0ab8933f6f407c3603b30a
Ramon Malaquias <malaquias@imd.ufrn.br>
2016-11-10 19:01:09 -0300

    Configuração do hibernate; Finalização das classes de domínio Pessoa e Usuario;

--- src/java/br/com/ufrn/bti/desktop/doinhome/domain/Pessoa.java
+++ src/java/br/com/ufrn/bti/desktop/doinhome/domain/Pessoa.java
@@ -1,5 +1,68 @@
 package br.com.ufrn.bti.desktop.doinhome.domain;
 
-public class Pessoa {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name="pessoa", schema="public")
+public class Pessoa {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_PESSOA")
+	@SequenceGenerator(name="SEQ_PESSOA", schema = "public", sequenceName="id_pessoa", allocationSize=1)
+	private int id;
+	public String cpf;
+	public String nome;
+	public Date dataNascimento;
+	public char sexo;
+	
+	public Pessoa(){
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}	
 }
--- src/java/br/com/ufrn/bti/desktop/doinhome/domain/Usuario.java
+++ src/java/br/com/ufrn/bti/desktop/doinhome/domain/Usuario.java
@@ -1,5 +1,83 @@
 package br.com.ufrn.bti.desktop.doinhome.domain;
 
-public class Usuario {
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+@SuppressWarnings("serial")
+@Entity
+@Table(name="usuario", schema="public")
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_USUARIO")
+	@SequenceGenerator(name="SEQ_USUARIO", schema = "public", sequenceName="id_usuario", allocationSize=1)
+	private int id;
+	private String login;
+	private String senha;
+	private String permissao;
+	private boolean ativo;	
+	
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name = "id_pessoa")
+	private Pessoa pessoa;
+	
+	public Usuario(String login, String senha) {
+		super();
+		this.login = login;
+		this.senha = senha;
+	}
+	
+	public Usuario(){
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getPermissao() {
+		return permissao;
+	}
+
+	public void setPermissao(String permissao) {
+		this.permissao = permissao;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}	
 }
+

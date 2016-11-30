3d53acee7df8fbd5a47612d0c59d61d37341dd39
Hamurabi Araújo <araujohamurabi@gmail.com>
2016-11-30 12:44:15 -0300

    Tela para cadastro e edição de tarefa

---
+++ src/java/br/com/ufrn/bti/desktop/doinhome/views/TarefasCadastroEdicaoViewController.java
@@ -0,0 +1,21 @@
+package br.com.ufrn.bti.desktop.doinhome.views;
+
+import br.com.ufrn.bti.desktop.doinhome.dominio.Usuario;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class TarefasCadastroEdicaoViewController {
+	@FXML
+	private TextField tfTarefaTitulo;
+	@FXML
+	private ComboBox<Usuario> cbTarefaResponsavel;
+	@FXML
+	private DatePicker dpTarefaPrazoFinal;
+	@FXML 
+	private Button btTarefaSalvar;
+	@FXML
+	private Button btTarefaCancelar; 
+}

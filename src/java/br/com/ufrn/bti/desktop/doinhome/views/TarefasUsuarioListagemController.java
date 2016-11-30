530a914ade8797fec8fe8492e0d359be5eaf1de6
Hamurabi Araújo <araujohamurabi@gmail.com>
2016-11-30 00:08:05 -0300

    View e controller da listagem de tarefas cadastradas

---
+++ src/java/br/com/ufrn/bti/desktop/doinhome/views/TarefasUsuarioListagemController.java
@@ -0,0 +1,50 @@
+package br.com.ufrn.bti.desktop.doinhome.views;
+
+import java.util.Date;
+
+import br.com.ufrn.bti.desktop.doinhome.dominio.Tarefa;
+import br.com.ufrn.bti.desktop.doinhome.dominio.Usuario;
+import br.com.ufrn.bti.desktop.doinhome.servico.TarefaService;
+import br.com.ufrn.bti.desktop.doinhome.servico.UsuarioService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class TarefasUsuarioListagemController {
+	private ObservableList<Tarefa> tarefasList = FXCollections.observableArrayList();
+	private UsuarioService usuarioService = new UsuarioService();
+	private TarefaService tarefaService =  new TarefaService();
+	private Usuario usuarioLogado;
+	private ContainerController containerController = new ContainerController();
+	
+	@FXML
+	private TableView<Tarefa> tvTarefas;
+	@FXML
+	private TableColumn<Tarefa, String> tcDescricaoTarefa;
+	@FXML 
+	private TableColumn<Tarefa, Date> tcDataFimTarefa;
+	@FXML
+	private TableColumn<Tarefa, String> tcAcoesTarefa;
+	
+
+	public TarefasUsuarioListagemController() {
+		tarefasList = FXCollections.observableList(tarefaService.listarTarefasPendentesUsuario(this.containerController.getUsuarioLogado()));
+	}
+	
+	public void setUsuarioLogado(Usuario u) {
+		this.usuarioLogado = u;
+	}
+	
+	@FXML
+    private void initialize() {
+		tcDescricaoTarefa.setCellValueFactory(cellData -> cellData.getValue().getDescricaoProperty());
+		tcDataFimTarefa.setCellValueFactory(cellData -> cellData.getValue().getDataFinalizacaoProperty());
+		System.out.println("Inicializando...");
+	}
+	
+	public void setContainerController(ContainerController c) {
+		this.containerController = c;
+	}
+}

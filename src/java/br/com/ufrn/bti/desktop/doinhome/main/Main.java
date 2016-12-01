a2bc8c8b64dc97b2c495e42cc9614443b27452fe
Hamurabi Araújo <araujohamurabi@gmail.com>
2016-12-01 00:06:24 -0300

    Main para mockar dados de uma tarefa para visualização

--- src/java/br/com/ufrn/bti/desktop/doinhome/main/Main.java
+++ src/java/br/com/ufrn/bti/desktop/doinhome/main/Main.java
@@ -1,8 +1,11 @@
 package br.com.ufrn.bti.desktop.doinhome.main;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import br.com.ufrn.bti.desktop.doinhome.dao.PessoaDAO;
+import br.com.ufrn.bti.desktop.doinhome.dao.TarefaDAO;
 import br.com.ufrn.bti.desktop.doinhome.dao.UsuarioDAO;
 import br.com.ufrn.bti.desktop.doinhome.dominio.Pessoa;
 import br.com.ufrn.bti.desktop.doinhome.dominio.Tarefa;
@@ -10,7 +13,7 @@ import br.com.ufrn.bti.desktop.doinhome.dominio.Usuario;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Pessoa p = new Pessoa();
 		Usuario u = new Usuario();
 		Tarefa t = new Tarefa();
@@ -23,14 +26,26 @@ public class Main {
 		u.setLogin("ramon");
 		u.setPessoa(p);
 		
-		t.setDescricao("Tarefa Teste");
+		String dataLimite = "2016-12-15";
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+		Date d = sdf.parse(dataLimite);
+		
+		t.setDescricao("Tarefa Legal");
+		t.setAtiva(true);
+		t.setDataCriacao(new Date());
+		t.setDataLimite(d);
+		t.setUsuario(u);
+		t.setValor(50);
 		
 		PessoaDAO pDao = new PessoaDAO();
 		UsuarioDAO uDao = new UsuarioDAO();
+		TarefaDAO tDao = new TarefaDAO();
 		
-		pDao.salvarOuAtualizar(p);
+//		pDao.salvarOuAtualizar(p);
 		
-		uDao.salvarOuAtualizar(u);
+//		uDao.salvarOuAtualizar(u);
+		
+		tDao.salvarOuAtualizar(t);		
 	}
 
 }

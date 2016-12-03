3fe591d80db35fa71280e4f6c5367ec770058e9d
Hamurabi Araújo <araujohamurabi@gmail.com>
2016-12-03 17:48:03 -0300

    Links para as novas telas adicionados

--- src/java/br/com/ufrn/bti/desktop/doinhome/views/ContainerController.java
+++ src/java/br/com/ufrn/bti/desktop/doinhome/views/ContainerController.java
@@ -6,13 +6,24 @@ import br.com.ufrn.bti.desktop.doinhome.dominio.Usuario;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class ContainerController {
 	private BorderPane container;
 	private Usuario usuarioLogado;
+	
+	@FXML
+	private Pane conteudo;
+	@FXML
+	private Hyperlink hlTodasTarefas;
+	@FXML
+	private Hyperlink hlNovaTarefa;
+	@FXML
+	private Hyperlink hlMinhasTarefas;
 	
 	public void start(Stage stage, Usuario usuarioLogado) {
 		try {
@@ -20,6 +31,7 @@ public class ContainerController {
             loader.setLocation(MainApp.class.getResource("Container.fxml"));
             container = (BorderPane) loader.load();
             Scene scene = new Scene(container);
+            conteudo = new Pane();
             
 			stage.setScene(scene);
 			stage.setTitle("DoInHome");
@@ -42,8 +54,8 @@ public class ContainerController {
 	public void mostrarListaTarefasUsuario(Usuario usuarioLogado) throws IOException{
 		FXMLLoader loader = new FXMLLoader();
         loader.setLocation(ContainerController.class.getResource("TarefasUsuarioListagem.fxml"));
-        AnchorPane conteudo = (AnchorPane) loader.load();
-        container.setCenter(conteudo);
+        this.conteudo.getChildren().clear();
+        this.conteudo.getChildren().add((AnchorPane) loader.load());
         
         TarefasUsuarioListagemViewController tarefasController = loader.getController();
         tarefasController.setUsuarioLogado(usuarioLogado);
@@ -51,9 +63,43 @@ public class ContainerController {
         tarefasController.setContainerController(this);
 	}
 	
+	@FXML
+	public void mostrarFormularioDeNovaTarefa() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(ContainerController.class.getResource("TarefasCadastroEdicao.fxml"));
+        this.conteudo.getChildren().clear();
+        this.conteudo.getChildren().add((AnchorPane) loader.load());
+	}
+	
+	@FXML 
+	public void mostrarCadastroDeNovoUsuario() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(ContainerController.class.getResource("UsuarioCadastroEdicao.fxml"));
+        this.conteudo.getChildren().clear();
+        this.conteudo.getChildren().add((AnchorPane) loader.load());
+	}
 	
 	@FXML
-	public void mostrarFormularioDeNovaTarefa(){
-		
+	public void mostrarListaDeUsuarios() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(ContainerController.class.getResource("UsuarioLista.fxml"));
+        this.conteudo.getChildren().clear();
+        this.conteudo.getChildren().add((AnchorPane) loader.load());
+	}
+	
+	@FXML 
+	public void mostrarListaDeTarefasDoDia() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(ContainerController.class.getResource("TarefasDoDia.fxml"));
+        this.conteudo.getChildren().clear();
+        this.conteudo.getChildren().add((AnchorPane) loader.load());
+	}
+	
+	@FXML
+	public void mostrarMinhastarefas() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(ContainerController.class.getResource("TarefasUsuarioListagem.fxml"));
+        this.conteudo.getChildren().clear();
+        this.conteudo.getChildren().add((AnchorPane) loader.load());
 	}
 }
